aeefd8a55a64141fdb31728944bf56ed139988b2
Segger87 <Segger87@gmail.com>
2018-04-24 16:57:21 +0100

    Console App that retrieves Lat Long, Country and Region of any given postcode

---
+++ Postcode/Postcode/PostCodeApiCalls.cs
@@ -0,0 +1,36 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Postcode
+{
+    class PostCodeApiCalls
+    {
+        public PostCodeDetails GetPostCodeDetails(string postcode)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var Client = new RestClient("http://api.postcodes.io/");
+            var request = new RestRequest("postcodes/" + postcode, Method.GET);
+            var postCodeData = Client.Execute<PostCodeAPIResponse>(request);
+            return postCodeData.Data.Result;
+        }
+    }
+
+  
+        public class PostCodeAPIResponse
+        {
+            public PostCodeDetails Result { get; set; }
+        }
+
+        public class PostCodeDetails
+        {
+            public override string ToString() { return string.Format("Country = {0}, Region = {1}, Long = {2}, Lat = {3}", Country, Region, Longitude, Latitude);  }
+            public string Country { get; set; }
+            public string Region { get; set; }
+            public string Longitude { get; set; }
+            public string Latitude { get; set; }
+        }
+}
---
+++ Postcode/Postcode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Postcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Write your postcode");
+            string postcode = Console.ReadLine().ToLower();
+            var apiCall = new PostCodeApiCalls();          
+            var returnPostcodeResults = apiCall.GetPostCodeDetails(postcode);
+
+            Console.WriteLine(returnPostcodeResults);
+
+            Console.ReadLine();
+        }
+    }
+}
